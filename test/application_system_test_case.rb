3405c452029884904c863f49bd96d1cb080a31a1
Alejandro Lahoz <alejandrolahozdefez@gmail.com>
2022-05-31 15:22:10 +0200

    first-commit

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,6 @@
+require "test_helper"
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  # Change :chrome with :headless_chrome to run tests without a browser
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+end
